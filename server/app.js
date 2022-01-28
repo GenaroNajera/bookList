deee8fb5b7d89e769be241251a3d07971ff3c82a
Genaro <genaronaj@gmail.com>
2022-01-27 17:49:18 -0800

    connected db. get and post routes with test db

--- server/app.js
+++ server/app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db.js'); // connects database to server
 
 app.use(cors());
+// app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
+app.get('/', (req, res, next) => {
+  pool.query('SELECT * FROM test', (err, result) => {
+    if(err) return next(err);
+    
+    res.send(result.rows);
+  });
+});
+
+app.post('/', (req, res) => {
+  const {name, description} = req.body;
+  pool.query('INSERT INTO test(name, description) VALUES($1, $2)', [name, description], (err, result) => {
+    if(err) return next(err);
+
+    console.log(result);
+    res.send('ok');
+  });
+});
+
+app.use((err, req, res, next) => { //error handler
+  res.status(500).send(err.message);
 });
 
 const port = process.env.port || 5000;
